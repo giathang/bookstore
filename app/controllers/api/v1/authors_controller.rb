d0d3f9fb746b280aaf1c367ccc08c67123dab4fb
giathang <gomez@vinova.sg>
2016-05-10 00:38:03 +0700

    create v1 author controller

---
+++ app/controllers/api/v1/authors_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::AuthorsController < ApplicationController
+  before_action :find_author, only: [:show, :update, :destroy]
+  skip_before_filter :verify_authenticity_token, :only => :create
+
+  def index
+    @authors = Author.all
+  end
+
+  def show
+  end
+
+  def create
+    @author = Author.create(author_params)
+  end
+
+  def update
+    @author.update(author_params)
+  end
+
+  def destroy
+    @author.destroy
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:name)
+  end
+
+  def find_author
+    @book = Author.find(params[:id])
+  end
+end
