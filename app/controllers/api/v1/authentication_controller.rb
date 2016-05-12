8f7b3616f68cd3ea2a649f764e1c739e548352bf
hungkhanh0709 <giathang.huflit@gmail.com>
2016-05-12 09:17:13 +0700

    create authenticate to check user

---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,23 @@
+class AuthenticationController < ApplicationController
+
+  # use authenticate to check user
+  def authenticate_user
+    user = User.find_for_database_authentication(email: params[:email])
+    if user.valid_password?(params[:password])
+      render json: payload(user)
+    else
+      render json: {errors: ['Invalid Username/Password']}, status: :unauthorized
+    end
+  end
+
+  private
+
+  # call payload
+  def payload(user)
+    return nil unless user && user.id
+    {
+        auth_token: JsonWebToken.encode({user_id: user.id}),
+        user: {id: user.id, email: user.email}
+    }
+  end
+end
