be93342ccbed9edc85f35ec2a31d44b989dce507
hungkhanh0709 <giathang.huflit@gmail.com>
2016-05-12 09:05:31 +0700

    create function get header , payload, signature in JWT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  attr_reader :current_user
+
+
+
+  private
+
+  # Get header
+  def http_token
+    @http_token ||= if request.headers['Authorization'].present?
+                      request.headers['Authorization'].slipt(' ').last
+                    end
+  end
+
+  # Get payload
+  def auth_token
+    @auth_token ||= JsonWebToken.decode(http_token)
+  end
+
+  # Get signature
+  def user_id_in_token?
+    http_token && auth_token && auth_token[:user_id].to_i
+  end
 end
